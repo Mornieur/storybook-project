778af6abfd9111487547538ef81374e178bc28b4
Mornieur <mariafernanda-coro@hotmail.com>
2023-04-25 22:06:34 -0300

    feat: create button example

---
+++ src/global/components/Button/index.tsx
@@ -0,0 +1,45 @@
+import React, { MouseEvent, ReactNode } from "react";
+import * as S from "./styles";
+import { themeBtn } from "./theme";
+
+export interface IButton {
+  children: ReactNode;
+  theme: "primary" | "secondary" | "save" | "exclude" | "edit";
+  // onClick?: any;
+  onClick?: (e: MouseEvent<HTMLElement>) => void;
+  className?: string;
+  type?: "submit" | "button" | "reset";
+  form?: any;
+  disabled?: boolean;
+  htmlFor?: string;
+  onKeyDown?: (e: MouseEvent<HTMLElement>) => void;
+}
+
+export const Button: React.FC<IButton> = ({
+  children,
+  theme,
+  className,
+  type,
+  onClick,
+  form,
+  disabled,
+  onKeyDown,
+}: IButton) => {
+  theme = theme as keyof typeof themeBtn;
+
+  return (
+    <S.Container
+      form={form}
+      type={type ? type : "button"}
+      themeBtn={themeBtn[theme]}
+      className={className ? className : ""}
+      onClick={onClick}
+      disabled={disabled}
+      onKeyDown={onKeyDown as any}
+    >
+      <S.SimpleContainer>
+        <p>{children}</p>
+      </S.SimpleContainer>
+    </S.Container>
+  );
+};
