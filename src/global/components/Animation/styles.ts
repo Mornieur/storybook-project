7263a81a326f2d783438dbf3f0cb2707ee3d3a2f
Mornieur <mariafernanda-coro@hotmail.com>
2023-04-25 22:06:12 -0300

    style: styling loading component

---
+++ src/global/components/Animation/styles.ts
@@ -0,0 +1,98 @@
+import styled from "styled-components";
+
+interface IContainer {
+  background: any;
+}
+
+export const Container = styled.div<IContainer>`
+  /* width: 100%;
+  height: 100%; */
+  position: absolute;
+  overflow: hidden;
+  max-width: 300px;
+  max-height: 400px;
+  width: 100%;
+  height: 100%;
+
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ background }) => background};
+
+  .rick-container {
+    display: flex;
+    justify-content: center;
+
+    div {
+      width: 100px;
+      height: 100px;
+      display: flex;
+      justify-content: center;
+      margin: 0 auto;
+      align-items: center;
+      position: relative;
+
+      img {
+        object-fit: contain;
+        width: 5rem;
+        height: 5rem;
+      }
+    }
+  }
+
+  .refresh {
+    padding: 10px;
+    position: absolute;
+    background: rgba(0, 0, 0, 0.4);
+    border-radius: 10px;
+    width: 20px;
+    height: 20px;
+    top: 10px;
+    right: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+  }
+
+  article {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    text-align: center;
+    font-size: 2rem;
+
+    display: flex;
+
+    h4 {
+      color: white;
+      padding-top: 1.3rem;
+    }
+  }
+
+  section {
+    margin-left: -2.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: left;
+    gap: 1rem;
+    padding-top: 1rem;
+    div {
+      color: white;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+`;
+export const LoadingContainer = styled.div`
+  width: 600px;
+  height: 400px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
