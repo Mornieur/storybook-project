a087c1af94517340da56025bb2b8d5329366efbe
Mornieur <mariafernanda-coro@hotmail.com>
2023-04-25 22:11:57 -0300

    style: styling main page

---
+++ src/styles/home.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
