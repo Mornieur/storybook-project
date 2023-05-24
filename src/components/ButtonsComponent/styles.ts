1131ae7497f4037635f7422f4ae0092fb3922b06
Mornieur <mariafernanda-coro@hotmail.com>
2023-05-24 15:03:47 -0300

    format: add prettier format

--- src/components/ButtonsComponent/styles.ts
+++ src/components/ButtonsComponent/styles.ts
@@ -1,9 +1,19 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
   justify-content: center;
   width: 100%;
   padding: 1rem;
+  gap: 1rem;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
+  width: 100%;
   gap: 1rem;
 `;
