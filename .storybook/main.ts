63d2e3fa0e9220b41b7d3934942ae664e077d84f
Mornieur <mariafernanda-coro@hotmail.com>
2023-05-24 15:02:39 -0300

    build: changing storybook settings

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,34 +1,33 @@
-import type { StorybookConfig } from "@storybook/nextjs";
-import path from "path";
-import "storybook-vscode-component/register";
+import type { StorybookConfig } from '@storybook/nextjs'
+import path from 'path'
+import 'storybook-vscode-component/register'
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
 
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "storybook-dark-mode",
-    "@storybook/addon-actions",
-    "@storybook/addon-viewport",
-    "@storybook/addon-styling",
-    "@storybook/addon-knobs",
-    "@storybook/addon-viewport",
-    "@storybook/addon-a11y",
-    "@storybook/addon-controls",
-    "@storybook/addon-backgrounds",
-    "@storybook/addon-toolbars",
-    "@storybook/addon-measure",
-    "@storybook/addon-outline",
-    "@storybook/addon-actions",
-    "@storybook/addon-mdx-gfm",
-    "storybook-addon-designs",
-    "@storybook/addon-highlight",
-    "storybook-css-modules",
-    "storybook-addon-gatsby",
-    "storybook-addon-jarle-monaco",
-    "storybook-addon-swc",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-dark-mode',
+    '@storybook/addon-actions',
+    '@storybook/addon-viewport',
+    '@storybook/addon-styling',
+    '@storybook/addon-knobs',
+    '@storybook/addon-viewport',
+    '@storybook/addon-a11y',
+    '@storybook/addon-controls',
+    '@storybook/addon-backgrounds',
+    '@storybook/addon-toolbars',
+    '@storybook/addon-measure',
+    '@storybook/addon-outline',
+    '@storybook/addon-mdx-gfm',
+    'storybook-addon-designs',
+    '@storybook/addon-highlight',
+    'storybook-css-modules',
+    'storybook-addon-gatsby',
+    'storybook-addon-jarle-monaco',
+    'storybook-addon-swc',
     // "@whitespace/storybook-addon-html",
     // "storybook-addon-root-attribute/register",
     // "storybook-addon-custom-event-broadcaster/dist",
@@ -39,53 +38,53 @@ const config: StorybookConfig = {
     // "@storybook/addon-google-analytics",
     // "storybook-addon-live-examples",
     {
-      name: "@storybook/addon-storysource",
+      name: '@storybook/addon-storysource',
       options: {
         rule: {
           // test: [/\.stories\.jsx?$/], This is default
-          include: [path.resolve(__dirname, "../src")],
+          include: [path.resolve(__dirname, '../src')]
         },
         loaderOptions: {
-          prettierConfig: { printWidth: 80, singleQuote: false },
-        },
-      },
+          prettierConfig: { printWidth: 80, singleQuote: false }
+        }
+      }
     },
 
     {
-      name: "@storybook/addon-docs",
+      name: '@storybook/addon-docs',
       options: {
         configureJSX: true,
         // sourceLoaderOptions: {
         //   injectStoryParameters: false,
         // },
         loaderOptions: {
-          injectStoryParameters: false,
-        },
-      },
-    },
+          injectStoryParameters: false
+        }
+      }
+    }
   ],
   framework: {
-    name: "@storybook/nextjs",
+    name: '@storybook/nextjs',
     options: {
-      nextConfigPath: path.resolve(__dirname, "../next.config.js"),
-    },
+      nextConfigPath: path.resolve(__dirname, '../next.config.js')
+    }
   },
   docs: {
-    autodocs: "tag",
-    defaultName: "Documentation",
+    autodocs: 'tag',
+    defaultName: 'Documentation'
   },
   refs: {
-    "package-name": {
-      disable: true,
-    },
+    'package-name': {
+      disable: true
+    }
   },
-  staticDirs: ["../public"],
+  staticDirs: ['../public'],
   features: {
-    storyStoreV7: true,
-  },
+    storyStoreV7: true
+  }
   // env: (config) => ({
   //   ...config,
   //   API_HOMOLOG: "testt",
   // }),
-};
-export default config;
+}
+export default config
--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,7 +1,7 @@
-import { addons } from "@storybook/addons";
-import { themes } from "@storybook/theming";
-import { darkTheme } from "./themes";
+import { addons } from '@storybook/addons'
+import { themes } from '@storybook/theming'
+import { darkTheme } from './themes'
 
 addons.setConfig({
-  theme: themes.normal,
-});
+  theme: themes.dark
+})
