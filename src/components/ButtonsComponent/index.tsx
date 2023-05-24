3069d7ddb24df59d0b3da6178ccec1faca1abfa0
Mornieur <mariafernanda-coro@hotmail.com>
2023-05-24 15:03:34 -0300

    feat: changing button component

--- src/components/ButtonsComponent/index.tsx
+++ src/components/ButtonsComponent/index.tsx
@@ -1,14 +1,18 @@
-import { Button } from "@/global/components/Button";
-import * as S from "./styles";
+import { Button } from '@/global/components/Button';
+import * as S from './styles';
+import { TextAnimation } from '@/global/components/TextAnimation';
 
 export const ButtonsComponent = () => {
   return (
     <S.Container>
-      <Button theme="primary">Primary</Button>
-      <Button theme="secondary">Secondary</Button>
-      <Button theme="save">Save</Button>
-      <Button theme="edit">Edit</Button>
-      <Button theme="delete">Delete</Button>
+      <TextAnimation type="title" isOpen={true} text="Buttons Examples" />
+      <S.ButtonsContainer>
+        <Button theme="primary">Primary</Button>
+        <Button theme="secondary">Secondary</Button>
+        <Button theme="save">Save</Button>
+        <Button theme="edit">Edit</Button>
+        <Button theme="delete">Delete</Button>
+      </S.ButtonsContainer>
     </S.Container>
   );
 };
