cae6c2a8683b3b7d0872ba694d4bc1aa464d882c
Mornieur <mariafernanda-coro@hotmail.com>
2023-06-10 20:13:03 -0300

    refactor: add prettier format

--- src/global/components/Button/index.tsx
+++ src/global/components/Button/index.tsx
@@ -1,37 +1,34 @@
-import React, { MouseEvent, ReactNode } from 'react'
-import * as S from './styles'
-import { themeBtn } from './theme'
+import React, { MouseEvent, ReactNode, KeyboardEvent } from 'react';
+import * as S from './styles';
+import { themeBtn } from './theme';
 
 export interface IButton {
-  children: ReactNode
-  theme: 'primary' | 'secondary' | 'save' | 'delete' | 'edit'
-  onClick?: (e: MouseEvent<HTMLElement>) => void
-  className?: string
-  type?: 'submit' | 'button' | 'reset'
-  form?: any
-  disabled?: boolean
-  htmlFor?: string
-  onKeyDown?: (e: MouseEvent<HTMLElement>) => void
+  children: ReactNode;
+  theme: 'primary' | 'secondary' | 'save' | 'delete' | 'edit';
+  onClick?: (e: MouseEvent<HTMLElement>) => void;
+  className?: string;
+  type?: 'submit' | 'button' | 'reset';
+  disabled?: boolean;
+  htmlFor?: string;
+  onKeyDown?: (e: KeyboardEvent<HTMLButtonElement>) => void;
 }
 
 export const Button: React.FC<IButton> = ({
   children,
-  theme,
-  className,
-  type,
+  theme = 'primary',
+  className = '',
+  type = 'button',
   onClick,
-  form,
-  disabled,
-  onKeyDown
+  disabled = false,
+  onKeyDown,
 }: IButton) => {
-  theme = theme as keyof typeof themeBtn
+  theme = theme as keyof typeof themeBtn;
 
   return (
     <S.Container
-      form={form}
-      type={type ? type : 'button'}
+      type={type}
       themeBtn={themeBtn[theme]}
-      className={className ? className : ''}
+      className={className}
       onClick={onClick}
       disabled={disabled}
       onKeyDown={onKeyDown as any}
@@ -40,5 +37,5 @@ export const Button: React.FC<IButton> = ({
         <p>{children}</p>
       </S.SimpleContainer>
     </S.Container>
-  )
-}
+  );
+};
