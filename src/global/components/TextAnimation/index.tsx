d05bd51969b6c21b31e65181ab879104269bb907
Mornieur <mariafernanda-coro@hotmail.com>
2023-06-16 22:46:53 -0300

    feat: renaming styling file

--- src/global/components/TextAnimation/index.tsx
+++ src/global/components/TextAnimation/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import * as S from './style';
+import * as S from './styles';
 
 interface ITextAnimation {
   type: 'title' | 'text';
